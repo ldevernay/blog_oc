ce595d2a2cbff0ff90685d69e9893928ace18394
ldevernay <boblehamster@yahoo.fr>
2017-05-29 14:41:13 +0200

    routes restrained

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
-Route::get('{n}', function($n) { return 'Je suis la page '.$n.' !'; });
+Route::get('{n}', function($n) {
+  return 'Je suis la page '.$n.' !';
+})->where('n', '[1-3]');
